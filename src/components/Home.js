009e5b2502502e3a99f28a559d2aa1f8156730fd
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-23 11:23:57 +0330

    work on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            Home
+  return (
+    <div className="container-fluid p-md-5">
+      <div className="row mx-md-5">
+        <div className="col-md-12 ">
+          <div className="form-group">
+            <label for="exampleInputEmail1">Github UserName</label>
+            <input  type="text" className="form-control"  placeholder="Enter github username ..."  />
+          </div>
+
+          <button className="btn btn-primary btn-block">
+            Submit
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
