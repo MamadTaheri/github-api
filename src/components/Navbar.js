5c9c132bf05d930bfb78f22e77c4feae5095238d
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-23 10:49:30 +0330

    update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <span>MY API </span>
+            <span className="h5">MOHAMMAD TAGERI </span>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
+                        <Link to="/" class="nav-link">Home</Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <Link to="/about" class="nav-link">About</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/contact" class="nav-link">Contact</Link>
                     </li>
                 </ul>
             </div>
