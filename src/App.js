cee553bf12e7ac03d431b5a4a1cda807b3e667a7
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-23 09:55:35 +0330

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <h1>App Component</h1>
     </div>
   );
