97aefcb5890aa6280bf5207c7983b26f7f96bf96
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-24 12:05:54 +0330

    change brand name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <span className="h5">MOHAMMAD TAGERI </span>
+            <span className="h5 text-primary">MTgithub </span>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
