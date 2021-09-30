a9a8063a95b49cd44fdbd09a3043551d6ffa46f0
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-30 13:47:42 +0330

    move Navbar to UI folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // Components
-import Navbar from './components/Navbar';
+import Navbar from './components/UI/Navbar';
 import Home from './components/Home';
 import About from './components/About'
 import Contact from './components/Contact'
