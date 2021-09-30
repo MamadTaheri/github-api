82aaeb1b99c9495fa9e3e50d05b128224f5b0284
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-30 14:08:01 +0330

    add new route to project

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
       <Switch>
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
+        <Route path="/user/:id" component={Contact} />
         <Route path="/" component={Home} />
       </Switch>
     </div>
