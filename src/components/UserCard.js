5dbe75f379929173375b386bd83c8c85e7986ab3
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-30 15:18:38 +0330

    update UserCard

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const User = ({data}) => {
 
     return (
-        <div className="col-md-3 m-md-3 m-1 p-1 shadow">
+        <div className="col-md-3 m-md-3 m-1 p-1 shadow text-center">
             <img className="img-fluid" src={data.avatar_url} alt={data.login} />
            <h5>{data.login}</h5>
-           <Link to=""></Link>
+           <Link to={`/user/${data.login}`} className="btn btn-block btn-primary">More</Link>
         </div>
     );
 };
