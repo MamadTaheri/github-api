8869bfd42c8c3975836843a730fa4dd0a5c5c36b
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-30 14:07:36 +0330

    add spinner and change User to UserCard

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const User = ({data}) => {
 
@@ -6,6 +7,7 @@ const User = ({data}) => {
         <div className="col-md-3 m-md-3 m-1 p-1 shadow">
             <img className="img-fluid" src={data.avatar_url} alt={data.login} />
            <h5>{data.login}</h5>
+           <Link to=""></Link>
         </div>
     );
 };
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import React, { useState } from "react";
-import User from '../User'
+import UserCard from '../UserCard'
+import spinner from '../../assets/spinner.gif'
 
 const Home = () => {
 
   const [data, setData] = useState([]);
   const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const url = "https://api.github.com/search/users?q=";
 
@@ -14,8 +16,10 @@ const Home = () => {
   }
 
   const searchHandler = () => {
+    setLoading(true);
     axios.get(url + text)
     .then(response => {
+      setLoading(false);
       setData(response.data);
     })
     .catch(error => console.log(error));
@@ -38,19 +42,26 @@ const Home = () => {
         </div>
       </div>
 
-      
-         { data.total_count >= 0 && 
-          <>
+      { loading &&  
           <div className="row"> 
-            <div className="col-md-12 pt-4">
-              <span className="badge-primary px-4 py-2 rounded-pill"> Total found users : {data.total_count}</span>
+            <div className="col-md-12 pt-4 text-center">
+               <img src={spinner} alt="spinner" />
             </div>  
-           </div>
-           <div className="row justify-content-center pt-4">
-             {data.items.map(user => <User data={user} key={user.id} />)}
-           </div>  
-          </> 
-         }
+          </div> 
+      }
+      
+      { data.total_count >= 0 && !loading && 
+      <>
+      <div className="row"> 
+        <div className="col-md-12 pt-4">
+          <span className="badge-primary px-4 py-2 rounded-pill"> Total found users : {data.total_count}</span>
+        </div>  
+        </div>
+        <div className="row justify-content-center pt-4">
+          {data.items.map(user => <UserCard data={user} key={user.id} />)}
+        </div>  
+      </> 
+      }
      
     </div>
   );
