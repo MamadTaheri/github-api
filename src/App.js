ec6ab4e8c481fef70b86ed573df0c6beba13ef7a
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-30 15:20:43 +0330

    refactor routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 // Components
-import Navbar from './components/UI/Navbar';
-import Home from './components/pages/Home';
-import About from './components/pages/About'
-import Contact from './components/pages/Contact'
+import Navbar from "./components/UI/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import User from "./components/pages/User";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Switch>
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/user/:id" component={Contact} />
-        <Route path="/" component={Home} />
+        <Route path="/about" exact>
+          <About />
+        </Route>
+        <Route path="/contact" exact>
+          <Contact />
+        </Route>
+        <Route path="/user/:login" exact>
+          <User />
+        </Route>
+        <Route path="/" exact>
+          <Home />
+        </Route>
       </Switch>
     </div>
   );
