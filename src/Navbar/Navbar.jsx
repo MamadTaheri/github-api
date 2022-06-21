6cbecd29f1298db67ffc533fc1674b15f978f292
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-22 00:07:14 +0430

    update Navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,49 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 import logo from "../assets/images/MtGithubFinderLogo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
-    
+  const [show, setShow] = useState(false);
+
   return (
     <div className="custom-navbar">
       <div className="leftSide">
         <img src={logo} />
-        <FaIcons.FaBars  />
+        <FaIcons.FaBars onClick={() => setShow(!show)} />
       </div>
-      <div className="rightSide">
-        <div className="links">
-          <Link to="/"> خانه </Link>
-          <Link to="/about">درباره ما</Link>
-          <Link to="/contact">تماس با ما</Link>
-        </div>
+      <div className={show ? "rightSide active" : "rightSide"}>
+        <ul>
+          {linksData.map((item) => (
+            <li key={item.id}>
+              <Link to={item.path} onClick={() => setShow(false)}>{item.title}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
-      {/* 
-       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <span className="h5 text-primary">
-                <img src={logo} width={200}/>
-            </span>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link to="/" className="nav-link">خانه</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/about" className="nav-link">درباره ما</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/contact" className="nav-link">تماس با ما</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav> 
-        */}
     </div>
   );
 };
 
 export default Navbar;
+
+const linksData = [
+  { id: 1, path: "/", title: "خانه" },
+  { id: 2, path: "/about", title: "درباره ما" },
+  { id: 3, path: "/contact", title: "تماس با ما" },
+];
