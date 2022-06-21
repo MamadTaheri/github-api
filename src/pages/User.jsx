2d4ac4181485fd143e2eea5c43fd9c5fa3401b65
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-22 01:03:05 +0430

    update User styles

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import spinner from "../assets/images/spinner.gif";
+import * as FaIcons from "react-icons/fa";
+import "./styles/User.css";
 
 const User = () => {
   const { login } = useParams();
@@ -32,39 +34,49 @@ const User = () => {
     </div>
   ) : (
     <div className="container p-md-5">
-        <div className="row">
-           <div className="col-md-12 p-1">
-              <Link className="btn btn-dark" to="/">Back to Search</Link>
-              <span>Hierable</span>
-           </div>
+      <div className="row">
+        <div className="col-md-12 p-1">
+          <Link className="btn btn-outline-dark" to="/">
+            <FaIcons.FaArrowAltCircleLeft />
+            <span className="ml-2">Back to Search</span>
+          </Link>
         </div>
-      <div className="row justify-content-center border rounded">
+      </div>
+      <div className="row justify-content-center border rounded shadow-lg">
         <div className="col-md-6 text-center">
-          <img
-            className="img-fluid img-rounded py-2"
-            src={user.avatar_url}
-            alt={user.login}
-          />
-          <h5 className="p-2">{user.login}</h5>
-          <h5 className="p-2">{user.location}</h5>
+          <div className="avatar-custom-container">
+            <img className="img-fluid" src={user.avatar_url} alt={user.login} />
+            <span className="badge badge-primary p-3">Hirable ✔</span>
+          </div>
         </div>
         <div className="col-md-6">
-          <h6 className="py-3">{user.bio}</h6>
+          <h5 className="py-2">Bio:</h5>
+          <h5 className="py-2">{user.bio}</h5>
+          <h5 className="py-2">Login: {user.login}</h5>
+          <h5 className="py-2">
+            website :
+            {user.blog ? (
+              <a
+                href={user.blog}
+                target="_blank"
+                rel="noreferrer"
+                className="ml-2"
+              >
+                {user.blog}
+              </a>
+            ) : (
+              <span className="text-danger ml-2 p-2">Website Not Found</span>
+            )}
+          </h5>
+          <h5 className="py-2">Location: {user.location}</h5>
           <a
-            className="btn btn-dark"
+            className="btn btn-success py-2"
             href={`https://github.com/${user.login}`}
             target="_blank"
             rel="noreferrer"
           >
             VISIT GITHUB
           </a>
-          <p className="py-2">Login : {user.login}</p>
-          <p>
-            website :{" "}
-            <a href={user.blog} target="_blank" rel="noreferrer"> 
-              {user.blog}
-            </a>
-          </p>
         </div>
       </div>
     </div>
