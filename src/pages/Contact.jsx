fc5e9362d4a669a8b44e1626e5cb2a5c4cd330f6
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-22 11:19:20 +0430

    update User page and Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,47 +1,76 @@
-import React from 'react';
+import React from "react";
+import "./styles/Contact.css";
 
 const Contact = () => {
-    return (
-        <div>
-            <div className="container rtl p-3 p-md-5">
-              <div className="row">
-                <div className="col-md-12 p-2 p-md-5">
-                <h2>تماس با من</h2>
-                <h5>جهت تماس با من میتوانید از مسیرهای ارتباطی زیر استفاده کنید:</h5>
-                <ul className="contact-list p-1 p-md-4">
-
-                    <li>
-                        <i className="fa fa-envelope-open-o" aria-hidden="true"></i>
-                        <span>ایمیل :</span>
-                        <span>mamad.taheri.68@gmail.com</span>
-                    </li>
-
-                    <li>
-                        <i className="fa fa-github" aria-hidden="true"></i>
-                        <span>لینکدین :</span>
-                        <a href="https://www.linkedin.com/in/mohammad-taheri-186101177/" target="_blank" rel="noreferrer">مشاهده لینک</a>
-                    </li>
-
-                    <li>
-                        <i className="fa fa-github" aria-hidden="true"></i>
-                        <span>گیت هاب :</span>
-                        <a href="https://github.com/MamadTaheri68" target="_blank" rel="noreferrer">مشاهده لینک</a>
-                    </li>
-
-                    <li>
-                        <i className="fa fa-telegram" aria-hidden="true"></i>
-                        <span>  تلگرام :</span>
-                        <a href="http://t.me/mohammad_taheri_1989" target="_blank" rel="noreferrer">مشاهده لینک</a>
-                    </li>
-
-                </ul>
-
-
+  return (
+    <div>
+      <div className="container rtl p-3 p-md-5 persian-font">
+        <div className="row">
+          <div className="col-md-12 p-2 p-md-5">
+            <div className="border-bottom-primary">
+              <span className="">تماس با من</span>
             </div>
+            <h5>
+              جهت تماس با من میتوانید از مسیرهای ارتباطی زیر استفاده کنید:
+            </h5>
+            <ul className="contact-list p-1 p-md-4">
+              <li>
+                <i className="fa fa-envelope-open-o" aria-hidden="true"></i>
+                <span>ایمیل :</span>
+                <h5>
+                  <a href="mailto:mamad.taheri.68@gmail.com">
+                    mamad.taheri.68@gmail.com
+                  </a>
+                </h5>
+              </li>
+
+              <li>
+                <i className="fa fa-github" aria-hidden="true"></i>
+                <span>لینکدین :</span>
+                <h5>
+                  <a
+                    href="https://www.linkedin.com/in/mohammad-taheri-186101177/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    www.linkedin.com/in/mohammad-taheri-186101177
+                  </a>
+                </h5>
+              </li>
+
+              <li>
+                <i className="fa fa-github" aria-hidden="true"></i>
+                <span>گیت هاب :</span>
+                <h5>
+                  <a
+                    href="https://github.com/MamadTaheri68"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    github.com/MamadTaheri68
+                  </a>
+                </h5>
+              </li>
+
+              <li>
+                <i className="fa fa-telegram" aria-hidden="true"></i>
+                <span> تلگرام :</span>
+                <h5>
+                  <a
+                    href="http://t.me/mohammad_taheri_1989"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    t.me/mohammad_taheri_1989
+                  </a>
+                </h5>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default Contact;
